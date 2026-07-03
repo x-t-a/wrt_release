57e39b487e5014eb95a1796641a62fe1309de743
星冉 <Jalapeno1868@outlook.com>
2026-07-03 13:17:02 +0800

    自定义插件

--- wrt_core/modules/feeds.sh
+++ wrt_core/modules/feeds.sh
@@ -27,8 +27,12 @@ update_feeds() {
     sed -i '/[[:space:]]small8[[:space:]]/d' "$FEEDS_PATH"
     sed -i '/[[:space:]]custom_feed[[:space:]]/d' "$FEEDS_PATH"
 
-    append_feed_if_missing "$FEEDS_PATH" "openwrt_bandix" "src-git openwrt_bandix https://github.com/timsaya/openwrt-bandix.git;main"
-    append_feed_if_missing "$FEEDS_PATH" "luci_app_bandix" "src-git luci_app_bandix https://github.com/timsaya/luci-app-bandix.git;main"
+    # append_feed_if_missing "$FEEDS_PATH" "openwrt_bandix" "src-git openwrt_bandix https://github.com/timsaya/openwrt-bandix-plus.git;main"
+    # append_feed_if_missing "$FEEDS_PATH" "luci_app_bandix" "src-git luci_app_bandix https://github.com/timsaya/uci-app-bandix-plus.git;main"
+    append_feed_if_missing "$FEEDS_PATH" "openwrt_bandix_plus" "src-git openwrt_bandix_plus https://github.com/timsaya/openwrt-bandix-plus.git;main"
+    append_feed_if_missing "$FEEDS_PATH" "luci_app_bandix_plus" "src-git luci_app_bandix_plus https://github.com/timsaya/luci-app-bandix-plus.git;main"
+    append_feed_if_missing "$FEEDS_PATH" "openwrt_mwan3" "src-git openwrt_mwan3 https://github.com/Ran-Xing/mwan3.git;main"
+    append_feed_if_missing "$FEEDS_PATH" "luci_app_mwan3" "src-git luci_app_mwan3 https://github.com/Ran-Xing/luci-app-mwan3.git;main"
 
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
         touch "$BUILD_DIR/include/bpf.mk"
